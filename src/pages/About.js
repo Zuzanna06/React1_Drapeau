d6afaca1beabc4ff94c1ce0c79b562777b574c52
Zuzanna <sich.zuzanna@gmail.com>
2023-07-25 09:49:02 +0200

    React1sur2

---
+++ src/pages/About.js
@@ -0,0 +1,38 @@
+import React from "react";
+import Navigation from "../components/Navigation";
+import Logo from "../components/Logo";
+
+const About = () => {
+  return (
+    <div>
+      <Logo />
+      <Navigation />
+      <h1>About</h1>
+      <br />
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo quasi nisi
+        tenetur voluptates dignissimos itaque rem earum. Nulla, a aperiam error
+        harum molestias, consequatur quo ab corrupti suscipit ut maxime nemo
+        facilis exercitationem soluta molestiae non, minus reprehenderit quidem!
+        Impedit hic quas et libero a corporis quos officia quibusdam veniam!
+      </p>
+      <br />
+      <p>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Laborum
+        nostrum itaque obcaecati ipsum illo quaerat quod cupiditate fugiat aut
+        eveniet tenetur consequuntur et magnam voluptatem vero, distinctio
+        quidem minima inventore quis iusto perspiciatis, modi odit dolorum.
+        Dolore maxime maiores sunt fuga deserunt enim libero reiciendis
+        inventore ipsa modi dignissimos cupiditate quasi eum, porro natus. Eaque
+        eligendi nobis quod enim optio nostrum voluptatem magnam quibusdam
+        itaque! Delectus sed quasi minima tempora quisquam suscipit numquam
+        consequatur accusantium. Alias laborum voluptatum velit aspernatur
+        aliquam repudiandae! Quibusdam dolor dolore asperiores nam hic alias
+        repudiandae animi officiis, recusandae voluptatibus unde, blanditiis quo
+        amet aliquam id.
+      </p>
+    </div>
+  );
+};
+
+export default About;
